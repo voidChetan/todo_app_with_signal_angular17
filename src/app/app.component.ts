942d23ed3ad9fc26767bfc8a24bae80df44250a5
codefirst2022 <codefirst2022@gmail.com>
2024-10-04 13:38:40 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal,Signal } from '@angular/core'; 
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'todo_app_with_signal_angular17';
+  
+  courseName =  "DOT NET";
+
+  firstName =  signal('Angular');
+
+  rollNo =  signal <number> (123)
+
+  demoName: Observable<string>  = of("");
+
+  fullName  = computed (() =>  this.fName() + " " + this.mName() + " "+ this.lName());
+
+  fName = signal('');
+  mName = signal('');
+  lName = signal('');
+
+
+
+
+  constructor() {
+    setTimeout(() => {
+      this.courseName = "React JS ";
+      this.firstName.set("Angular 17.3")
+    }, 5000);
+  }
+  changeName() {
+    this.firstName.set("React")
+  }
+  changeFName(value: string) {
+    this.fName.set(value)
+  }
+  changeMName(value: string) {
+    this.mName.set(value)
+  }
+  changeLName(value: string) {
+    this.lName.set(value)
+  }
+
+  changeCouse() {
+    this.courseName = "React JS "
+  }
 }
